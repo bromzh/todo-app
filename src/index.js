9c32078e307ee1bd58f0af6c598a3e67a619d266
Belitamart <45999401+Belitamart@users.noreply.github.com>
2019-04-03 18:39:57 +0300

    Add files via upload
    Задачи добавляются.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ function main() {
   console.log('Todo app');
 
   // Put your code below
+
+//function of array out
+  function toDoListOut() {
+    var doList=document.getElementById('doList');
+    var tempData = '';
+    for (var i=0; i<x.length; i++) {
+      tempData+='<p>'+x[i]+'</p>';
+    }
+    doList.innerHTML=tempData;
+  }
+
+//add to array
+  document.getElementById('addInput').onclick=addToList;
+  var x=[];
+  function addToList() {
+    var inputText=document.getElementById('input').value;
+    x.push(inputText);
+
+    toDoListOut();
+
+    // console.log(x);
+  }
 }
 
 main();
