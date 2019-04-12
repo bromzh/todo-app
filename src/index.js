49ccf7bb58c88721f8b38c4bfd97b591e24162bd
Belitamart <belitamart@gmail.com>
2019-04-12 13:04:06 +0300

    Пытаюсь удаление замутить

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ function main() {
 
     // Put your code below
 
+    //Создание элементов в списке
 
     let doList = document.getElementById('doList');                //Записываем в переменную содержимое списка
     let doListArray = [];                                                  //Создаем массив
@@ -13,14 +14,30 @@ function main() {
     function addToList() {
         let inputText = document.getElementById('input').value;    //Создаем переменную и записываем туда содержимое строки ввода
         doListArray.push(inputText);                                       //Добавляем в массив содержимое перемнной inputText
+
         let newToDoLi = document.createElement(`li`);              //Назначаем переменную для создания строк
-        newToDoLi.innerHTML = doListArray[doListArrayCounter];             //Записываем в переменную строки содержимое элемента массива
+        newToDoLi.id = `line${doListArrayCounter}`
+
+        newToDoLi.innerHTML = `<input id="checkBox${doListArrayCounter}" type="checkbox">
+            ${doListArray[doListArrayCounter]}<button id="deleteLine${doListArrayCounter}"
+            onclick="f1()">Удалить</button>`;                           //Создаем чекбокс и записываем в переменную строки содержимое элемента массива
+
         doList.appendChild(newToDoLi);                                     //Добавляем в список строку с содержимым массива
 
         doListArrayCounter++;                                              //Увеличиваем счетчик массива на 1 для следующей записи
 
+
         /*console.log(doListArray);*/
+
+
     }
+
+    //Удаление элементов из списка
+
+    function f1() {
+        alert(`Сдохни бля`);
+    }
+
 }
 
 main();
